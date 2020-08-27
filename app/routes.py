d3235ecc629dff07ee2cd679e2c39bbd7000342b
ankitwhoelse <ankitvinod@gmail.com>
2020-08-27 11:27:22 -0400

    Fin lab01-2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-        return "Hello dude!"
+        user = {'nom': 'Ankit'}
+        publications = [
+                {
+                        'auteur': {'nom': 'Jean'},
+                        'corps': "J'aime rien!"
+                },
+                {
+                        'auteur': {'nom': 'John'},
+                        'corps': "J'hais tout!"
+                }
+        ]
+        return render_template('index.html', titre='Acceuil', utilisateur=user, publications=publications)
