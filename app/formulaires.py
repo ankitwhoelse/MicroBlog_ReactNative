6ba722a156b9404e43c10792af89d29076b8bd60
ankitwhoelse <ankitvinod@gmail.com>
2020-09-01 14:04:47 -0400

    Fin lab02

---
+++ app/formulaires.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class FormulaireEtablirSession(FlaskForm):
+    nom = StringField('Nom', validators=[DataRequired(message="Nom est un champ oubligatoire")])
+    mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired(message="Mot de passe est un champ oubligatoire")])
+    se_souvenir_de_moi = BooleanField('Se souvenir de moi')
+    soumettre = SubmitField('Etablir une session')
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.formulaires import FormulaireEtablirSession
 
 @app.route('/')
 @app.route('/index')
 def index():
-        user = {'nom': 'Ankit'}
-        publications = [
-                {
-                        'auteur': {'nom': 'Jean'},
-                        'corps': "J'aime rien!"
-                },
-                {
-                        'auteur': {'nom': 'John'},
-                        'corps': "J'hais tout!"
-                }
-        ]
-        return render_template('index.html', titre='Acceuil', utilisateur=user, publications=publications)
+    user = {'nom': 'Ankit'}
+    publications = [
+        {
+            'auteur': {'nom': 'Jean'},
+            'corps': "J'aime rien!"
+        },
+        {
+            'auteur': {'nom': 'John'},
+            'corps': "J'hais tout!"
+        }
+    ]
+    return render_template('index.html', titre='Acceuil', utilisateur=user, publications=publications)
+
+
+@app.route('/etablir_session', methods=['GET', 'POST'])
+def etablir_session():
+    formulaire = FormulaireEtablirSession()
+    if formulaire.validate_on_submit():
+            flash('Etablir une session par utilisateur {}, se_souvenir_de_moi ={}'.format(formulaire.nom.data, formulaire.se_souvenir_de_moi.data))
+            return redirect('/index')
+    return render_template('etablir_session.html', titre='Etablir une session', formulaire=formulaire)
