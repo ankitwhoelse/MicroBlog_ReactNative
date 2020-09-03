0012bcda646f2d346cfa155a26b986fe2439e092
Jean-François <biz667@info.cgg>
2020-09-03 09:58:58 -0400

    Fin labo3

--- petits_gazouillis.py
+++ petits_gazouillis.py
@@ -1,2 +1,7 @@
-from app import app
+from app import app, db
+from app.modeles import Utilisateur, Publication
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'U': Utilisateur, 'P': Publication}
 
