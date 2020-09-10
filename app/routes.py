5a5c44f3c2347a50a7824f9b730ea989e7870b9f
Jean-François <biz667@info.cgg>
2020-09-10 10:11:05 -0400

    Fin labo04

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,21 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.formulaires import FormulaireEtablirSession
+from app.modeles import Utilisateur
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nom': 'Ankit'}
-    publications = [
-        {
-            'auteur': {'nom': 'Jean'},
-            'corps': "J'aime rien!"
-        },
-        {
-            'auteur': {'nom': 'John'},
-            'corps': "J'hais tout!"
-        }
-    ]
-    return render_template('index.html', titre='Acceuil', utilisateur=user, publications=publications)
+    
+    utilisateurs = Utilisateur.query.all()
+
+    return render_template('index.html', titre='Acceuil', utilisateurs=utilisateurs)
+
 
 
 @app.route('/etablir_session', methods=['GET', 'POST'])
 def etablir_session():
+
     formulaire = FormulaireEtablirSession()
     if formulaire.validate_on_submit():
             flash('Etablir une session par utilisateur {}, se_souvenir_de_moi ={}'.format(formulaire.nom.data, formulaire.se_souvenir_de_moi.data))
--- petits_gazouillis.py
+++ petits_gazouillis.py
@@ -1,7 +1,39 @@
-from app import app, db
+from app import app, db, modeles
 from app.modeles import Utilisateur, Publication
+import os, csv
+
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'U': Utilisateur, 'P': Publication}
 
+
+print("Denarrage petit gazouillis")
+
+@app.before_first_request
+def initialisation():
+    print("initialisation")
+    tables= app.config['BD_TABLES_EFFACER']
+    for table in tables:
+        requete = "delete from {}".format(table)
+        print(requete)
+        db.session.execute(requete)
+    db.session.commit()
+
+    tables = app.config['BD_TABLES_CREER']
+    racine = os.path.abspath(os.path.dirname(__file__))
+    for table in tables:
+        fichier = 'csv/' + table + '.csv'
+        if os.path.exists(racine + '/' + fichier):
+            source = os.path.join(racine, fichier)
+
+            print("===" + table + "===")
+            with open(source) as fichier_csv:
+                lecteur_csv = csv.reader(fichier_csv, delimiter=',')
+                for ligne in lecteur_csv:
+                    element = modeles.get_modele(table, ligne, racine)
+                    print (element)
+
+                    if element is not None:
+                        db.session.add(element)
+                    db.session.commit()
