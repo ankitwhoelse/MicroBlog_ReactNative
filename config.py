a50709e698a9fa9617b307e3d7cb35e58363e2a8
Jean-François <biz667@info.cgg>
2020-09-24 11:17:13 -0400

    Fin lab07

--- config.py
+++ config.py
@@ -10,3 +10,10 @@ class Config(object):
     #BD_TABLES_EFFACER = os.environ.get('BD_TABLES_EFFACER') or [ 'tache' , 'message' , 'notification' , 'followers', 'publication', 'utilisateur']
     BD_TABLES_EFFACER = os.environ.get('BD_TABLES_EFFACER') or ['publication', 'utilisateur']
     BD_TABLES_CREER = os.environ.get('BD_TABLES_CREER') or [ 'utilisateur', 'publication']
+
+    MAIL_SERVER = "localhost"
+    MAIL_PORT = 8025
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@exemple.com']
